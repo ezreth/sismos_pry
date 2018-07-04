216bed0bc9943aa04a8a422b97f3ecdafee872b2
Diego López <diego.loport@gmail.com>
2018-07-03 19:47:00 -0500

    Se crea funcion transpone

--- src/dataseguros.py
+++ src/dataseguros.py
@@ -10,47 +10,47 @@ def main():
     print("KARINA MOLINA")
     print("DIEGO LÓPEZ")
 
-    e1 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA'], ['COD', 'IMPUESTO', 'TAMANIO', 'UTILIDADES'], ["INGRESOS"], "1"]
-    #crea_escenarios(e1)
+    e1 = [['ANIO', 'COD',  'SECTOR'], ['IMPUESTO', 'TAMANIO', 'UTILIDADES', 'REGION', 'PROVINCIA'], ["INGRESOS"], "1"]
+    crea_escenarios(e1,"","")
 
     e2 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA'], ['COD', 'IMPUESTO', 'TAMANIO', 'INGRESOS'], ["UTILIDADES"], "2"]
-    crea_escenarios(e2)
+    #crea_escenarios(e2,"","")
 
     e3 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA'], ['COD', 'UTILIDADES', 'TAMANIO', 'INGRESOS'], ["IMPUESTO"], "3"]
-    crea_escenarios(e3)
+    #crea_escenarios(e3,"","")
 
     e4 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA'], ['COD', 'IMPUESTO', 'TAMANIO'], ["INGRESOS","UTILIDADES"], "4"]
-    crea_escenarios(e4)
+    #crea_escenarios(e4,"","")
 
     e5 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA'], ['COD', 'UTILIDADES', 'TAMANIO'], ["INGRESOS","IMPUESTO"], "5"]
-    crea_escenarios(e5)
+    #crea_escenarios(e5,"","")
 
     e6 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA'], ['COD', 'TAMANIO', 'INGRESOS'], ["UTILIDADES","IMPUESTO"], "6"]
-    crea_escenarios(e6)
+    #crea_escenarios(e6,"","")
 
     e7 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA'], ['COD', 'TAMANIO'], ["INGRESOS","UTILIDADES", "IMPUESTO"], "7"]
-    crea_escenarios(e7)
+    #crea_escenarios(e7,"","")
 
     e8 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA','TAMANIO'], ['COD', 'IMPUESTO', 'UTILIDADES'], ["INGRESOS"], "8"]
-    crea_escenarios(e8)
+    #crea_escenarios(e8,"","")
 
     e9 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA', 'TAMANIO'], ['COD', 'IMPUESTO', 'INGRESOS'], ["UTILIDADES"], "9"]
-    crea_escenarios(e9)
+    #crea_escenarios(e9,"","")
 
     e10 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA', 'TAMANIO'], ['COD', 'UTILIDADES', 'INGRESOS'], ["IMPUESTO"], "10"]
-    crea_escenarios(e10)
+    #crea_escenarios(e10,"","")
 
     e11 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA', 'TAMANIO'], ['COD', 'IMPUESTO'], ["INGRESOS", "UTILIDADES"], "11"]
-    crea_escenarios(e11)
+    #crea_escenarios(e11,"","")
 
     e12 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA', 'TAMANIO'], ['COD', 'UTILIDADES'], ["INGRESOS", "IMPUESTO"], "12"]
-    crea_escenarios(e12)
+    #crea_escenarios(e12,"","")
 
     e13 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA', 'TAMANIO'], ['COD', 'INGRESOS'], ["UTILIDADES", "IMPUESTO"], "13"]
-    crea_escenarios(e13)
+    #crea_escenarios(e13,"","")
 
     e14 = [['ANIO', 'REGION', 'SECTOR', 'PROVINCIA', 'TAMANIO'], ['COD'], ["INGRESOS","UTILIDADES", "IMPUESTO"], "14"]
-    crea_escenarios(e14)
+    #crea_escenarios(e14,"","")
 
     print("-FIN-")
 
--- src/escenarios.py
+++ src/escenarios.py
@@ -1,7 +1,9 @@
 from read_csv import *
 from splitdata import *
+import pandas as pd
+import json
 
-def crea_escenarios(lista):
+def crea_escenarios(lista,filtro,valor):
 
     print('-Crea escenario ' + lista[3] + '-')
 
@@ -11,15 +13,77 @@ def crea_escenarios(lista):
 
     df = df.drop(lista[1], axis=1)
 
+    df = filtra_escenario('COD','F',df)
+
+
+
+
+
+    #print(df)
+
     set_group = df.groupby(lista[0])[lista[2]].apply(lambda x: x.astype(int).sum())
+    set_group.sort_values(by=['ANIO'])
 
-
-    training, test = get_train_test_inds(set_group)
-
-    crea_csv(training,'etraining' + lista[3])
-    crea_csv(test, 'etest' + lista[3])
+    #print(set_group)
 
 
 
+    #set_group = set_group.loc[set_group['COD'].isin(['C', 'G'])]
+
+    transpone(set_group,4,'F')
 
 
+    #set_group = filtra_escenario('COD', 'G', set_group)
+
+    #training, test = get_train_test_inds(set_group)
+
+    #crea_csv(training,'etraining' + lista[3])
+    #crea_csv(test, 'etest' + lista[3])
+
+
+def filtra_escenario(campo,valor,df):
+    df = df.loc[df[campo] == valor]
+    return df
+
+def transpone(df,rango,iden):
+
+
+    str_json = ''
+
+    for indice_fila, fila in df.iterrows():
+        campo = indice_fila[0]
+        itera = indice_fila[2]
+        str_json = str_json + '{"' + itera + '": "' + str(campo) + '"'
+
+
+        for x in range(rango + 1):
+            anio = campo - x
+            if x == 0:
+                canio = "ANIO"
+            else:
+                canio = "ANIO-" + str(x)
+            valor = fitradf(df, anio)
+            if valor == None:
+                valor = 0
+            str_json = str_json + ', "' + canio + '": "' + str(valor) + '"'
+
+
+        str_json = str_json + '}, ' #\\ ' +'\n'
+
+
+    str_json = str_json[:-2]
+    str_json = '[' + str_json + ']'
+
+    #print(str_json)
+
+    df = pd.read_json(str_json, orient='records')
+    #print(df)
+    crea_csv(df,'E' + iden + str(rango))
+
+
+
+def fitradf(df, anio):
+
+    filtro = df.query('ANIO == ' + str(anio))
+    for a, b in filtro.iterrows():
+        return b[0]
